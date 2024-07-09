831e83df0d1a83cf0e5843364b24a9a18a6a7f88
quinlin217 <quinlin.217@gmail.com>
2024-07-09 20:21:46 +0700

    Update practice5.sql

--- practice5.sql
+++ practice5.sql
@@ -16,6 +16,15 @@ inner join texts
 on emails.email_id=texts.email_id
 
 -- ex3:
+select 
+  b.age_bucket,
+  round(100.0*sum(case when activity_type='send' then a.time_spent else 0 end)/sum(a.time_spent),2) as send_perc,
+  round(100.0*sum(case when activity_type='open' then a.time_spent else 0 end)/sum(a.time_spent),2) as open_perc
+from activities as a
+inner join age_breakdown as b 
+on a.user_id=b.user_id
+where a.activity_type<>'chat'
+group by b.age_bucket
 
 -- ex4:
 with supercloud_cust as (
